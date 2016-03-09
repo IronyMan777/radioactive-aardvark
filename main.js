ce50fea8360e6421c4b6c25cbb5cb13aa499018a
Irony Man <ironyman777@gmail.com>
2016-03-09 13:14:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 // Doop!
 
+int Width = 800;
+int Height = 400;
+int framerate = 60
+
 var font1 = createFont("Times New Roman");
+
+void setup(){
+    size(Width, Height);
+    frameRate(framerate);
+};
 
 void draw(){
 
